f9cd49ec70862e1f65c181062da755457278e1f6
Sancho Tech <munashebmachisi@gmail.com>
2025-09-14 01:35:46 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.1.7",
+  version: "1.0.0",
   updateZipUrl: "https://github.com/sanchotech/SANCHO-XMD/archive/refs/heads/main.zip",
 };
 
